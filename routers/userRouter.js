a2a4ab809aa42d73f8d8b6c201e61d071ff71434
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2020-09-02 02:44:20 +0900

    Updated Commits with PassportJS, CSS

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -11,7 +11,10 @@ const userRouter = express.Router();
 
 userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile, editProfile);
+
+userRouter.post(routes.changePassword, changePassword);
 userRouter.get(routes.changePassword, changePassword);
+
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
