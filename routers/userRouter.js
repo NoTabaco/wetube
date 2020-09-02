dccd2c70347805da87478b3fd52e7cd3e684cafe
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2020-09-03 00:26:55 +0900

    Updated Commits with PassportJS

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,14 +6,15 @@ import {
   editProfile,
   changePassword,
 } from "../controllers/userController";
+import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get(routes.users, users);
-userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile);
 
-userRouter.post(routes.changePassword, changePassword);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.post(routes.changePassword, onlyPrivate, changePassword);
+userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 
 userRouter.get(routes.userDetail(), userDetail);
 
